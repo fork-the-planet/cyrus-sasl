19289d272a29ef6c6197a3d60aa90d78faafe328
ellie timoney <ellie@fastmail.com>
2026-07-15 11:23:31 +1000

    docsrc/conf.py: cyrus-imapd 3.12.3, 3.10.3, 3.8.7 released

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -416,22 +416,22 @@ epub_exclude_files = ['search.html']
 # When this is updated, you may also need to update the version and release
 # definitions listed above to stay up to date.
 rst_prolog = """
-.. |imap_last_stable_version| replace:: 3.10.2
+.. |imap_last_stable_version| replace:: 3.10.3
 .. |imap_last_stable_branch| replace:: `cyrus-imapd-3.10`
-.. |imap_last_stable_next_version| replace:: 3.10.2 + patches
-.. |imap_current_stable_version| replace:: 3.12.2
+.. |imap_last_stable_next_version| replace:: 3.10.3 + patches
+.. |imap_current_stable_version| replace:: 3.12.3
 .. |imap_current_stable_branch| replace:: `cyrus-imapd-3.12`
-.. |imap_current_stable_next_version| replace:: 3.12.2 + patches
-.. |imap_latest_development_version| replace:: 3.13.3
+.. |imap_current_stable_next_version| replace:: 3.12.3 + patches
+.. |imap_latest_development_version| replace:: 3.13.6
 .. |imap_latest_development_branch| replace:: master
 .. |sasl_current_stable_version| replace:: 2.1.28
 .. |imap_stable_release_notes| raw:: html
 
-    <a href="3.12/x/3.12.2.html">3.12.2</a>
+    <a href="3.12/x/3.12.3.html">3.12.3</a>
 
 .. |imap_development_release_notes| raw:: html
 
-    <a href="https://www.cyrusimap.org/dev/imap/download/release-notes/3.13/x/3.13.3.html">3.13.3</a>
+    <a href="https://www.cyrusimap.org/dev/imap/download/release-notes/3.13/x/3.13.6.html">3.13.6</a>
 
 """
 
